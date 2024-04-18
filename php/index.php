9cdb5456be84cc1bab559bdce818d7acedb30dde
ferzian <m.ferzian09@gmail.com>
2024-04-18 10:46:15 +0700

    php keun

--- php/index.php
+++ php/index.php
@@ -2,192 +2,215 @@
 // manggil koneksi database
 include "koneksi.php";
 
+
+// Jika tombol ditekan
+if (isset($_POST['redirect'])) {
+  // Pindahkan pengguna ke halaman lain
+  header("Location: halaman_edit.php");
+  exit;
+}
+
+//bacground image
+$monas = '../Website Warisan Budaya/image/monas.jpg';
+$rumahGadang = '../Website Warisan Budaya/image/rumahGadang.jpg';
+$museumMulawarman = '../Website Warisan Budaya/image/museummulawarman.jpg';
+$bentengRotterdam = '../Website Warisan Budaya/image/bentengrotterdam.jpg';
+$bentengFortDuBus = '../Website Warisan Budaya/image/bentengfortdubus.jpg';
+$museumSumba = '../Website Warisan Budaya/image/museumsumba.jpg';
+
+echo '<style> .card-img-hover monas { background-image: url("' . $monas . '"); }</style>';
 ?>
 
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Warisan Budaya</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Warisan Jawa</title>
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet" />
+
+  <!-- Feather Icons -->
+  <script src="https://unpkg.com/feather-icons"></script>
+
+  <!-- My Styles -->
+  <link rel="stylesheet" href="../Website Warisan Budaya/CSS/styles.css" />
 </head>
 
 <body>
+  <!-- Navbar Start -->
+  <nav class="navbar">
+    <a href="#" class="navbar-logo">Warisan<span>Budaya</span></a>
 
-    <div class="container">
-        <div class="card mt-3">
-            <div class="card-header bg-dark text-white">
-                Data Warisan Budaya
-            </div>
-            <div class="card-body">
-
-                <!-- Button trigger modal tambah -->
-                <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#modalTambah">
-                    Tambah Data
-                </button>
-                <table class="table table-bordered table-striped table-hover">
-                    <tr>
-                        <th>No.</th>
-                        <th>Nama</th>
-                        <th>Kategori</th>
-                        <th>Lokasi</th>
-                        <th>Aksi</th>
-                    </tr>
-
-                    <?php
-
-                    // persiapan menampilkan data
-                    $no = 1;
-                    $tampil = mysqli_query($koneksi, "SELECT * FROM situs_budaya ORDER BY id DESC");
-                    while ($data = mysqli_fetch_array($tampil)):
-                        ?>
-
-                        <tr>
-                            <td>
-                                <?= $no++ ?>
-                            </td>
-                            <td>
-                                <?= $data['nama'] ?>
-                            </td>
-                            <td>
-                                <?= $data['kategori'] ?>
-                            </td>
-                            <td>
-                                <?= $data['lokasi'] ?>
-                            </td>
-                            <td>
-                                <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $no ?>">Ubah</a>
-                                <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $no ?>">Hapus</a>
-                            </td>
-                        </tr>
-
-                        <!-- Modal Ubah Start-->
-                        <div class="modal fade" id="modalUbah<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false"
-                            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Ubah Data Warisan Budaya
-                                        </h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <form method="POST" action="crud.php">
-                                        <input type="hidden" name='id' value="<?= $data['id']?>">
-                                        <div class="modal-body">
-                                            <div class="mb-3">
-                                                <label class="form-label">Nama</label>
-                                                <input type="text" class="form-control" name="nama" value="<?= $data['nama']?>"
-                                                    placeholder="Masukkan Nama Situs Budaya!">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label class="form-label">Kategori</label>
-                                                <select class="form-select" name="kategori">
-                                                    <option value="<?= $data['kategori']?>"></option>
-                                                    <option value="bangunan">Bangunan</option>
-                                                    <option value="museum">Museum</option>
-                                                </select>
-                                            </div>
-                                            <div class="mb-3">
-                                                <label class="form-label">Lokasi</label>
-                                                <input type="text" class="form-control" name="lokasi" value="<?= $data['lokasi']?>"
-                                                    placeholder="Masukkan Lokasi Situs Budaya!">
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="submit" class="btn btn-primary" name="bubah">Ubah</button>
-                                            <button type="button" class="btn btn-danger"
-                                                data-bs-dismiss="modal">Batal</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Modal Ubah End -->
-
-                        <!-- Modal Hapus Start-->
-                        <div class="modal fade" id="modalHapus<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false"
-                            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Konfirmasi Hapus Data
-                                        </h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <form method="POST" action="crud.php">
-                                        <input type="hidden" name='id' value="<?= $data['id']?>">
-                                        <div class="modal-body">
-                                            <h5 class="text-center"> Apakah anda yakin akan menghapus data ini? <br><span class="text-danger"><?= $data['nama']?> - <?=$data['lokasi'] ?> </span></h5>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="submit" class="btn btn-primary" name="bhapus">Hapus</button>
-                                            <button type="button" class="btn btn-danger"
-                                                data-bs-dismiss="modal">Batal</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Modal Hapus End -->
-
-                    <?php endwhile; ?>
-                </table>
-
-
-                <!-- Modal Tambah Start -->
-                <div class="modal fade" id="modalTambah" data-bs-backdrop="static" data-bs-keyboard="false"
-                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Tambah Data Warisan Budaya</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <form method="POST" action="crud.php">
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label class="form-label">Nama</label>
-                                        <input type="text" class="form-control" name="nama"
-                                            placeholder="Masukkan Nama Situs Budaya!">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Kategori</label>
-                                        <select class="form-select" name="kategori">
-                                            <option value="bangunan">Bangunan</option>
-                                            <option value="museum">Museum</option>
-                                        </select>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Lokasi</label>
-                                        <input type="text" class="form-control" name="lokasi"
-                                            placeholder="Masukkan Lokasi Situs Budaya!">
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-primary" name="bsimpan">Simpan</button>
-                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <!-- Modal Tambah End -->
-            </div>
-        </div>
-
+    <div class="navbar-nav">
+      <ul>
+        <li><a href="home">Beranda</a></li>
+        <li><a href="#">Galeri</a></li>
+        <li><a href="#">Tentang Kami</a></li>
+        <li>
+          <a href="#informasi" id="chevron-down">Kategori<i data-feather="chevron-down"></i>
+            <ul class="dropdown">
+              <li><a href="#">Musium</a></li>
+              <li><a href="#">Tarian</a></li>
+              <li><a href="#">Bangunan</a></li>
+            </ul>
+          </a>
+        </li>
+      </ul>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <div class="navbar-extra">
+      <a href="#" id="search"><i data-feather="search"></i></a>
+      <a href="#" id="humburger-menu"><i data-feather="menu"></i></a>
+    </div>
+  </nav>
+  <!-- Navbar End -->
 
+  <!-- Hero Section Start -->
+
+  <section class="hero" id="home">
+    <main class="content">
+      <h1>Mari Mengenal Warisan <span>Budaya</span>.</h1>
+      <p>Berbahaya Jika Tidak Ber<span>budaya</span></p>
+    </main>
+  </section>
+
+  <!-- Hero Section End -->
+
+  <!-- Card Start -->
+  <section class="cards">
+    <article class="card">
+      <div class="card-img"></div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $monas; ?>')">
+        </div>
+      </a>
+
+      <div class="card-info">
+        <span class="card-category">Bangunan</span>
+        <h3 class="card-title">Monumen Nasional, DKI Jakarta</h3>
+        <p class="card-desc">Monas adalah ikon kota Jakarta, Indonesia. Monumen setinggi 132 meter ini didirikan untuk
+          mengenang perjuangan bangsa Indonesia dalam merebut kemerdekaan dari penjajahan Belanda.</p>
+      </div>
+    </article>
+
+    <article class="card">
+      <div class="card-img"></div>
+      </div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $rumahGadang; ?>')">
+        </div>
+      </a>
+      <div class="card-info">
+        <span class="card-category">Bangunan</span>
+        <h3 class="card-title">Rumah Gadang, Sumatra Barat</h3>
+        <p class="card-desc">Rumah Gadang adalah rumah tradisional Minangkabau yang dapat ditemui di wilayah
+          Minangkabau, Sumatra Barat, Indonesia. Rumah ini memiliki ciri khas atap melengkung seperti tanduk kerbau yang
+          indah.</p>
+      </div>
+    </article>
+
+    <article class="card">
+      <div class="card-img"></div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $museumMulawarman; ?>')">
+        </div>
+      </a>
+      <div class="card-info">
+        <span class="card-category">Museum</span>
+        <h3 class="card-title">Museum Mulawarman, Kalimantan Timur</h3>
+        <p class="card-desc">Terletak di Kota Tenggarong, Kalimantan Timur, Indonesia, Museum Mulawarman adalah museum
+          yang menggambarkan sejarah dan kekayaan budaya suku Dayak dan kehidupan di Kalimantan Timur.</p>
+      </div>
+    </article>
+  </section>
+
+  <section class="cards">
+    <article class="card">
+      <div class="card-img"></div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $bentengRotterdam; ?>')">
+        </div>
+      </a>
+      <div class="card-info">
+        <span class="card-category">Bangunan</span>
+        <h3 class="card-title">Benteng Rotterdam, Sulawesi Selatan</h3>
+        <p class="card-desc">Dikenal sebagai Benteng Makassar atau Benteng Ujung Pandang yang terletak di Kota Makassar,
+          Sulawesi Selatan, Indonesia. Benteng ini dibangun oleh Belanda pada abad ke-17 sebagai benteng pertahanan.</p>
+      </div>
+    </article>
+
+    <article class="card">
+      <div class="card-img"></div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $bentengFortDuBus; ?>')">
+        </div>
+      </a>
+      <div class="card-info">
+        <span class="card-category">Bangunan</span>
+        <h3 class="card-title">Benteng Fort Du Bus, Papua Barat</h3>
+        <p class="card-desc">Terletak di Bukittinggi, Sumatra Barat, Indonesia, benteng ini adalah sisa-sisa peninggalan
+          Belanda yang dibangun pada abad ke-19.</p>
+      </div>
+    </article>
+
+    <article class="card">
+      <div class="card-img"></div>
+      <a href="#">
+        <div class="card-img-hover" style="background-image: url('<?php echo $museumSumba; ?>')">
+        </div>
+      </a>
+      <div class="card-info">
+        <span class="card-category">Museum</span>
+        <h3 class="card-title">Museum Sumba, Nusa Tenggara Timur</h3>
+        <p class="card-desc">Terletak di Waingapu, Sumba Timur, Nusa Tenggara Timur, Indonesia, museum ini adalah tempat
+          untuk mengeksplorasi kebudayaan dan warisan budaya unik dari pulau Sumba.</p>
+      </div>
+    </article>
+  </section>
+  <!-- Card End -->
+
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <button type="submit" class="btn btn-secondary mb-3" name="redirect"> Edit CRUD</button>
+  </form>
+
+  <!-- Kritik dan Saran Start -->
+  <div class="container-saran">
+    <form action="">
+      <h1>Kritik dan Saran</h1>
+      <label for="">Nama:</label>
+      <input type="text" name="nama" id="nama"> <br>
+      <label for="">Kritik dan Saran:</label>
+      <input type="text" name="saran" id="saran">
+    </form>
+  </div>
+  <!-- Kritik dan Saran End -->
+
+  <!-- Footer Start -->
+  <div class="footer">
+    <p>&copy;copyright m-punk 2024</p>
+  </div>
+  <!-- Footer End -->
+
+  <!-- Feather Icons -->
+  <script>
+    feather.replace();
+  </script>
+
+
+  <script src="../Website Warisan Budaya/JS/script.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
